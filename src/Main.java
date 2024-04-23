86891a97b793ac3633ee576dc62d03d162da375f
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-04-23 17:38:55 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int amount = 990; // Пополняемая сумма
+        int summa = 200; // Баланс счёта
+        int bonus = amount / 100; // Начисляемые бонусы
+
+        if (amount < 1000) {
+            bonus = 0;
+        }
+        int x = amount + summa + bonus;
+        System.out.println( "Ваш баланс телефона:" + x);
+
+    }
+}
